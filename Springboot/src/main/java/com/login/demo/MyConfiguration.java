8d56d94fd55acc422ef9f2bc5182d18a2dfb346e
MarkBryanCalupig <120764484+MarkBryanCalupig@users.noreply.github.com>
2023-02-23 17:54:52 +0800

    Update MyConfiguration.java

--- Springboot/src/main/java/com/login/demo/MyConfiguration.java
+++ Springboot/src/main/java/com/login/demo/MyConfiguration.java
@@ -1,21 +1,59 @@
 package com.login.demo;
 
-import org.springframework.context.annotation.Bean;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration(proxyBeanMethods = false)
-public class MyConfiguration {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(final CorsRegistry registry) {
-                System.out.println("\n\n\n\n __________________________ \n\n\n\n\n Adding the Configurations");
-                registry.addMapping("/**").allowedMethods("*").allowedHeaders("*");
-            }
-        };
+import java.io.IOException;
+
+@Configuration
+public class CorsConfiguration implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "*");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        //response.setHeader("Access-Control-Expose-Headers","yourCustomHeaderIfExist");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
     }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+    }
+
+    @Override
+    public void destroy() {
+    }
+
 }
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// @Configuration(proxyBeanMethods = false)
+// public class MyConfiguration {
+
+//     @Bean
+//     public WebMvcConfigurer corsConfigurer() {
+//         return new WebMvcConfigurer() {
+//             @Override
+//             public void addCorsMappings(final CorsRegistry registry) {
+//                 System.out.println("\n\n\n\n __________________________ \n\n\n\n\n Adding the Configurations");
+//                 registry.addMapping("/**").allowedMethods("*").allowedHeaders("*");
+//             }
+//         };
+//     }
+// }
